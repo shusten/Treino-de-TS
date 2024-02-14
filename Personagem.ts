95cf30deeeb1710737b2a02ca97189f81423cc78
Luis Villas-Boas <shusten@hotmail.com>
2024-02-13 23:55:00 -0300

    Testando instâncias

--- Personagem.ts
+++ Personagem.ts
@@ -6,6 +6,26 @@ class Personagem {
     defesa: number = 0;
 }
 
-let person: Personagem;
-person = new Personagem();
-console.log("Person :>> ", person);
+let luis: Personagem;
+
+luis = new Personagem();
+luis.nome = 'Luis';
+luis.ataque = 10;
+luis.defesa = 20;
+luis.energia = 50;
+luis.vida = 100;
+console.log("Luis :>> ", luis);
+
+let grazi: Personagem = new Personagem();
+
+grazi.nome = 'Grazi';
+grazi.ataque = 80;
+grazi.defesa = 70;
+grazi.energia = 90;
+grazi.vida = 40;
+
+console.log("Grazi :>> ", grazi);
+
+
+
+
