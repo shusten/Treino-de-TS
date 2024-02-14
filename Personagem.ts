1b08d0ee71491681fc173f3ec157afb5c8bd37bc
Luis Villas-Boas <shusten@hotmail.com>
2024-02-14 03:55:52 -0300

    Typescript POO stydies

--- Personagem.ts
+++ Personagem.ts
@@ -1,29 +1,22 @@
-class Personagem {
-    nome: string = '';
-    energia: number = 0;
-    vida: number = 0;
-    ataque: number = 0;
-    defesa: number = 0;
+class Personagem {   
+    constructor(
+        public nome: string,
+        public energia: number,
+        public vida: number,
+        public ataque: number,
+        public defesa: number,
+    ) {}
 }
 
 let luis: Personagem;
 
-luis = new Personagem();
-luis.nome = 'Luis';
-luis.ataque = 10;
-luis.defesa = 20;
-luis.energia = 50;
-luis.vida = 100;
+luis = new Personagem('Luis', 50, 100, 10, 20);
 console.log("Luis :>> ", luis);
 
-let grazi: Personagem = new Personagem();
 
-grazi.nome = 'Grazi';
-grazi.ataque = 80;
-grazi.defesa = 70;
-grazi.energia = 90;
-grazi.vida = 40;
+let grazi: Personagem;
 
+grazi = new Personagem('Grazi', 90, 40, 80, 70);
 console.log("Grazi :>> ", grazi);
 
 
