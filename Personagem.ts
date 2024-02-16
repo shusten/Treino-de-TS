a93dba63c394692258097f49227e408a7ae5319f
Luis Villas-Boas <shusten@hotmail.com>
2024-02-15 23:58:44 -0300

    Testando menu

--- Personagem.ts
+++ Personagem.ts
@@ -10,17 +10,34 @@ class Personagem {
     ) {}
 }
 
+
 let luis: Personagem;
-
-luis = new Personagem('Luis', 50, 100, 10, 20);
-console.log("Luis :>> ", luis);
-
-
 let grazi: Personagem;
 
+let keyboard = prompt();
+let option: number = 0;
+
+while (option != 9) {
+    console.log("+==============Personagem==============+");
+    console.log("|1. Treinar ataque                     |");
+    console.log("|2. Treinar defesa                     |");
+    console.log("|3. Imprimir atributos                 |");
+    console.log("|9. Sair                               |");
+    console.log("+======================================+");
+
+    option = +keyboard("Escolha uma opção");
+
+    switch (option) {
+        case 1:
+            luis.ataque += 2;
+            break;
+    
+        default:
+            break;
+    }
+}
+
+luis = new Personagem('Luis', 50, 100, 10, 20);
 grazi = new Personagem('Grazi', 90, 40, 80, 70);
-console.log("Grazi :>> ", grazi);
-
-
 
 
