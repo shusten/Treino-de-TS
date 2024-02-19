0d3e45c57580a387c095d79210f6e48f8137ca88
Luis Villas-Boas <shusten@hotmail.com>
2024-02-19 00:09:20 -0300

    Improving game algorithm

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import Personagem from "./Personagem";
 import prompt from "prompt-sync";
 
-let luis: Personagem = new Personagem('Luis', 50, 100, 10, 20);
-let grazi: Personagem = new Personagem('Grazi', 90, 40, 80, 70);
+let person: Personagem = new Personagem('Personagem', 50, 100, 10, 20);
+/* let grazi: Personagem = new Personagem('Grazi', 90, 40, 80, 70); */
 
 let keyboard = prompt();
 let option: number = 0;
@@ -11,7 +11,9 @@ while (option != 9) {
     console.log("+==============Personagem==============+");
     console.log("|1. Treinar ataque                     |");
     console.log("|2. Treinar defesa                     |");
-    console.log("|3. Imprimir atributos                 |");
+    console.log("|3. Descansar                          |");
+    console.log("|4. Entrar em batalha                  |");
+    console.log("|8. Imprimir atributos                 |");
     console.log("|9. Sair                               |");
     console.log("+======================================+");
 
@@ -19,12 +21,26 @@ while (option != 9) {
 
     switch (option) {
         case 1:
-            luis.ataque += 2;
+            person.ataque += Math.random() * 7;
+            person.energia -= Math.random() * 10;
+            break;
+        case 2:
+            person.defesa += Math.random() * 5;
+            person.energia -= Math.random() * 10;
             break;
         case 3:
-            console.log("Luis :>> ", luis);
+            person.energia += Math.random() * 10;
+            break;
+        case 4:
+            person.energia -= Math.random() * 100;
+            if (person.energia < 0) {
+                console.log('Ops! Você morreu!');
+            }
+            break;
+        case 8:
+            console.log("Person :>> ", person);
             break;
         default:
-            break;
+        break;
     }
 }
