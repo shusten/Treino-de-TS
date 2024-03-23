b1486b0386556ae0a55637c7b6d48270fbf1469d
Luis Villas-Boas <shusten@hotmail.com>
2024-03-22 23:57:03 -0300

    Raname variables

--- Personagem.ts
+++ Personagem.ts
@@ -1,7 +1,7 @@
 export default class Personagem {   
     constructor(
         public _nome: string,
-        public energia: number,
+        public _energia: number,
         public vida: number,
         public ataque: number,
         public defesa: number,
@@ -27,7 +27,7 @@ export default class Personagem {
         "Guerreiro: \n" + 
         "\nNome: " +
         this._nome + 
-        ("\nEnergia: " + this.energia.toFixed(1)) +
+        ("\nEnergia: " + this._energia.toFixed(1)) +
         ("\nAtaque: " + this.ataque.toFixed(1)) +
         ("\nDefesa: " + this.defesa.toFixed(1))
         );
@@ -35,7 +35,7 @@ export default class Personagem {
 
     public treinarAtaque(): void {
         this.ataque += this.randomizar(7);
-        this.energia -= this.randomizar(10);
+        this._energia -= this.randomizar(10);
         if(this.defesa > 100) {
             this.defesa = 0;
         }
@@ -43,27 +43,27 @@ export default class Personagem {
 
     public treinarDefesa(): void{
         this.defesa += this.randomizar(5);
-        this.energia -= this.randomizar(10);
+        this._energia -= this.randomizar(10);
         if(this.defesa > 100) {
             this.defesa = 0;
         }
     }
 
     public descansar(hora: number): void{
-        this.energia += hora * this.randomizar(10);
-        if (this.energia > 100) {
-            this.energia = 100;
+        this._energia += hora * this.randomizar(10);
+        if (this._energia > 100) {
+            this._energia = 100;
         } 
     }
 
     public batalhar(): number {
         let desgaste : number = this.randomizar(10);
-        this.energia -= desgaste;
-        return this.energia;
+        this._energia -= desgaste;
+        return this._energia;
     }
 
     public isDead(): boolean {
-        return this.energia < 0;
+        return this._energia < 0;
     }
 
     randomizar(fator: number): number {
