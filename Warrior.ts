39e89dd51ced1ac62b11be4191539d7bbc9ce7c1
Luis Villas-Boas <shusten@hotmail.com>
2024-03-22 23:55:48 -0300

    Add class Warrior

---
+++ Warrior.ts
@@ -0,0 +1,19 @@
+import { Util } from "./Util";
+
+class Warrior {
+    private _nome: string;
+    private _armadura: number;
+    private _vidaMaxima: number;
+    private _vidaAtual: number;
+    private _forca: number;
+    private _agilidade: number;
+
+    constructor(nome: string) {
+        this._nome = nome;
+        this._armadura = Util.randomizar(1_000, 10_000);
+        this._vidaMaxima = Util.randomizar(200, 10_000);
+        this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
+        this._forca = Util.randomizar(100, 1_000);
+        this._agilidade = Util.randomizar(100, 10_000);
+    }
+}
