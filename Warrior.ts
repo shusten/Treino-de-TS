ce979b323f53f842625a82068dea5a2c0c74267f
Luis Villas-Boas <shusten@hotmail.com>
2024-03-23 23:35:44 -0300

    Modify attributes

--- Warrior.ts
+++ Warrior.ts
@@ -12,8 +12,8 @@ class Warrior {
         this._nome = nome;
         this._armadura = Util.randomizar(1_000, 10_000);
         this._vidaMaxima = Util.randomizar(200, 10_000);
-        this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
+        this._vidaAtual = Util.randomizar(20, this._vidaMaxima);
         this._forca = Util.randomizar(100, 1_000);
-        this._agilidade = Util.randomizar(100, 10_000);
+        this._agilidade = Util.randomizar(100, 1_000);
     }
 }
