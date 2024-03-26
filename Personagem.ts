cc990655512a8ed9674c884bd3620692912166b7
Luis Villas-Boas <shusten@hotmail.com>
2024-03-25 23:54:25 -0300

    Applying inheritance

--- Personagem.ts
+++ Personagem.ts
@@ -1,72 +1,18 @@
-export default class Personagem {   
-    constructor(
-        public _nome: string,
-        public _energia: number,
-        public vida: number,
-        public ataque: number,
-        public defesa: number,
-    ) {}
-
     // SEMPRE QUE PENSAR EM LER DADOS NA CLASSE - USE PARÂMETRO
     // SEMPRE QUE PENSAR EM IMPRIMIR NA CLASSE - USE RETORNO
 
+    import { Util } from "./Util";
     
-    public get nome() : string {
-        return this._nome;
-    }
-
+    export class Personagem {   
+        protected _nome: string;
+        protected _armadura: number;
+        protected _vidaMaxima: number;
+        protected _vidaAtual: number;
     
-    public set nome(nome : string) {
-        this._nome = nome;
-    }
-    
-    
-
-    public status(): string {
-        return (
-        "Guerreiro: \n" + 
-        "\nNome: " +
-        this._nome + 
-        ("\nEnergia: " + this._energia.toFixed(1)) +
-        ("\nAtaque: " + this.ataque.toFixed(1)) +
-        ("\nDefesa: " + this.defesa.toFixed(1))
-        );
-    }
-
-    public treinarAtaque(): void {
-        this.ataque += this.randomizar(7);
-        this._energia -= this.randomizar(10);
-        if(this.defesa > 100) {
-            this.defesa = 0;
+        constructor(nome: string) {
+            this._nome = nome;
+            this._armadura = Util.randomizar(100, 1_000);
+            this._vidaMaxima = Util.randomizar(100, 1_000);
+            this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
         }
-    }
-
-    public treinarDefesa(): void{
-        this.defesa += this.randomizar(5);
-        this._energia -= this.randomizar(10);
-        if(this.defesa > 100) {
-            this.defesa = 0;
-        }
-    }
-
-    public descansar(hora: number): void{
-        this._energia += hora * this.randomizar(10);
-        if (this._energia > 100) {
-            this._energia = 100;
-        } 
-    }
-
-    public batalhar(): number {
-        let desgaste : number = this.randomizar(10);
-        this._energia -= desgaste;
-        return this._energia;
-    }
-
-    public isDead(): boolean {
-        return this._energia < 0;
-    }
-
-    randomizar(fator: number): number {
-        return Math.random() * fator;
-    }
 }
