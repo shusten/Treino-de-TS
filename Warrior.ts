66cdf8dc674ac41bb6f81ac8317dc36d8d486ce7
Luis Villas-Boas <shusten@hotmail.com>
2024-04-01 01:15:29 -0300

    Aplying inheritance in the Warrior class

--- Warrior.ts
+++ Warrior.ts
@@ -1,18 +1,16 @@
+import { Personagem } from './Personagem';
 import { Util } from "./Util";
 
-class Warrior {
-    private _nome: string;
-    private _armadura: number;
-    private _vidaMaxima: number;
-    private _vidaAtual: number;
+class Warrior extends Personagem{
     private _forca: number;
     private _agilidade: number;
 
     constructor(nome: string) {
-        this._nome = nome;
+        super(nome);
         this._armadura = Util.randomizar(1_000, 10_000);
         this._vidaMaxima = Util.randomizar(200, 10_000);
         this._vidaAtual = Util.randomizar(20, this._vidaMaxima);
+        this._vidaAtual = 100;
         this._forca = Util.randomizar(100, 1_000);
         this._agilidade = Util.randomizar(100, 1_000);
     }
