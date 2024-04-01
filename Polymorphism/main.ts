4becf8f1638a86260d5377c38ce1648abdae97b2
Luis Villas-Boas <shusten@hotmail.com>
2024-04-01 19:39:59 -0300

    Testing instanceOf operator

--- Polymorphism/main.ts
+++ Polymorphism/main.ts
@@ -1,13 +1,16 @@
 import { Mage } from "./src/Mage";
 import { Priest } from './src/Priest';
-import { Warrior } from './src/Warrior';
 import { Personagem } from './src/Personagem';
 
 let mage: Personagem = new Mage("Luis mage");
 let priest: Personagem = new Priest("Covra Priest");
-let warrior: Personagem = new Warrior("Carlos warriror");
 
-console.log("mage :>> ", mage);
-console.log("priest :>> ", priest);
-console.log("warrior :>> ", warrior);
+const personagens: Personagem[] = [];
 
+personagens.push(mage);
+personagens.push(priest);
+
+personagens.forEach((p) => 
+    console.log(p instanceof Mage? p.atacar: (p as Priest).atacar())
+    
+);
--- Polymorphism/src/Priest.ts
+++ Polymorphism/src/Priest.ts
@@ -14,4 +14,8 @@ export class Priest extends Personagem {
         this._espirito = Util.randomizar(100, 1_000);
         this._versatilidade = Util.randomizar(100, 1_000);
     }
+
+    public atacar(): string{
+        return "Ataque do padre";
+    }
 }
